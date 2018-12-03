b26204d5d8cfad6d7cf4e9010a12dc6397299a4e
ZacharyVasey <39927165+ZacharyVasey@users.noreply.github.com>
2018-12-02 21:17:45 -0600

    Update arithmeticLogicUnitTest.py

--- arithmeticLogicUnitTest.py
+++ arithmeticLogicUnitTest.py
@@ -4,23 +4,67 @@ from postalu import PostALU
 
 def main():
     # Register 1
-    register_one = 6
+    register_one = [6, 7, 9, 64, 6, 2, 1, 35, 44, 8, 55, 31, '']
     # Register 2
-    register_two = 12
+    register_two = [4, 3, 10, 2, 3, 6, 4, 74, 62, 12, 7, 3, '']
     # Immediate
-    immediate = 1
+    immediate = ['', '', '', '', '', '', '', '', 86, 255, '', '', '']
+    # Shift
+    shift = ['', '', 3, 4, '', '', '', 3, '', '', 1, 2, '']
     # OpcodeString
-    opcodeString = "ADD"
+    opcodeString = ["ADD", "SUB", "LSL", "LSR", "AND", "ORR", "EOR", "ASR", "ADDI", "SUBI", "MOVK", "MOVZ", "NOP"]
+
     # Create Pre ALU
     preALU = PreALU()
     # Create ALU
     ALU = ArithmeticLogicUnit()
     # Create post ALU unit
     postALU = PostALU()
-    # ALU Computer
-    ALU.compute(opCodeStr, Reg1, Reg2)
-    # assign output to post ALU
-    postALU.setContent(ALU.getOutput())
+
+    # index to help with testing
+    index = 0
+    # Expected result
+    expectedResult = 0
+
+    # ALU Computer TEST ADD
+    ALU.compute(opcodeString[index], register_one[index], register_two[index])
+    print opcodeString[index] + ", " + str(register_one[index]) + ", " + str(register_two[index])
+    print "Result: " + str(ALU.getOutput())
+    expectedResult = register_one[index] + register_two[index]
+    print "Expected: " + str(expectedResult) + "\n"
+    index = index + 1
+
+    # ALU Computer TEST SUB
+    ALU.compute(opcodeString[index], register_one[index], register_two[index])
+    print opcodeString[index] + ", " + str(register_one[index]) + ", " + str(register_two[index])
+    print "Result: " + str(ALU.getOutput())
+    expectedResult = register_one[index] - register_two[index]
+    print "Expected: " + str(expectedResult) + "\n"
+    index = index + 1
+
+    # ALU Computer TEST LSL
+    ALU.compute(opcodeString[index], register_one[index], shift[index])
+    print opcodeString[index] + ", " + str(register_one[index]) + ", " + str(shift[index])
+    print "Result: " + str(ALU.getOutput())
+    expectedResult = (register_one[index] % (1 << 64)) << shift[index]
+    print "Expected: " + str(expectedResult) + "\n"
+    index = index + 1
+
+    # ALU Computer TEST LSR
+    ALU.compute(opcodeString[index], register_one[index], shift[index])
+    print opcodeString[index] + ", " + str(register_one[index]) + ", " + str(shift[index])
+    print "Result: " + str(ALU.getOutput())
+    expectedResult = (register_one[index] % (1 << 64)) >> shift[index]
+    print "Expected: " + str(expectedResult) + "\n"
+    index = index + 1
+
+    # ALU Computer TEST AND
+    ALU.compute(opcodeString[index], register_one[index], register_two[index])
+    print opcodeString[index] + ", " + str(register_one[index]) + ", " + str(register_two[index])
+    print "Result: " + str(ALU.getOutput())
+    expectedResult = register_one[index] & register_two[index]
+    print "Expected: " + str(expectedResult) + "\n"
+    index = index + 1
 
 if __name__== "__main__":
 	main()
